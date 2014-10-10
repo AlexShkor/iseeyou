1b93dbf15f0640489615f1fb2d56742877d28945
Alexey Kulik <akulik@paralect.com>
2014-10-11 02:11:07 +0300

    VK users ranking

---
+++ ISeeYou.VkRanking/Models/RankedProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISeeYou.VkRanking.Models
+{
+    public class RankedProfile
+    {
+        public RankedProfile(long id, int startRank)
+        {
+            Id = id;
+            Rank = startRank;
+        }
+
+        public int Rank { get; set; }
+        public long Id { get; set; }
+    }
+}
