fb8c6251acb984775723bb8225671282c6873c77
Alex Shkor <alex.shkor@gmail.com>
2014-10-22 18:31:50 +0300

    deploy photo workers 10 units

--- src/ISeeYou/ViewServices/TrackingMarksViewService.cs
+++ src/ISeeYou/ViewServices/TrackingMarksViewService.cs
@@ -3,6 +3,7 @@ using ISeeYou.Databases;
 using ISeeYou.Platform.ViewServices;
 using ISeeYou.Views;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 
 namespace ISeeYou.ViewServices
 {
@@ -20,8 +21,19 @@ namespace ISeeYou.ViewServices
 
         public DateTime? GetSourceMark(int userId)
         {
-            var mark = Items.FindOneById("source" + userId);
+            var mark = Items.FindOneById(BuildSourceMarkId(userId));
             return mark != null ? mark.Tracked : (DateTime?)null;
+        }
+
+        public void SaveSourceMark(int userId, DateTime traked)
+        {
+            Items.Update(Query<TrackingMark>.EQ(x => x.Id, BuildSourceMarkId(userId)),
+                Update<TrackingMark>.Set(x => x.Tracked, traked), UpdateFlags.Upsert);
+        }
+
+        private string BuildSourceMarkId(int userId)
+        {
+            return "source" + userId;
         }
     }
 }
